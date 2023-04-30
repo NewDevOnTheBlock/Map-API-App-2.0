93b8d90cdadae5b0c6dcfaea8fcc849b5869c7a9
Pierce <piercedeanda@gmail.com>
2023-04-30 15:19:47 -0700

    fixed App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,10 @@
 <template>
-<<<<<<< HEAD
-  <main class="container">
+  <div class="container">
     <div class="map">
       <Map :lat="lat" :lon="lon" />
     </div>
     <div class="search">
-      <Search @position="position($event)"/>
-    </div>
-  </main>
-</template>
-
-<script lang="ts">
-import Map from './components/Map.vue';
-  import Search from './components/Search.vue';
-
-  export default {
-    name: "App",
-    components: {
-      Map,
-      Search,
-    },
-    data() {
-      return {
-        lat: 0,
-        lon: 0
-      };
-    },
-    methods: {
-      position(event: any) {
-        this.lat = event.lat;
-        this.lon = event.lon;
-      }
-    },
-  };
-</script>
-
-<style lang="scss">
-
-  * {
-    margin: 0;
-    padding: 0;
-  }
-
-  .container {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    
-    .map, 
-    .search {
-      width: 50%;
-      height: 100vh;
-    }
-  }
-
-  .search {
-    background-color: rgb(31, 31, 31);
-  }
-
-  body {
-    -ms-overflow-style: none; /* IE, edge */
-    scrollbar-width: none; /* firefox */
-    overflow-y: scroll;
-    &::-webkit-scrollbar {
-      display: none; /* chorme, safari, opera */
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    .container {
-      flex-direction: column;
-      
-      .map, 
-      .search {
-        width: 100%;
-        height: 50vh;
-      }
-    }
-
-    .list {
-      height: calc(100vh - (50vh + 146px));
-      overflow-y: scroll;
-=======
-  <div class="container">
-    <div class="map">
-      <Map />
-    </div>
-    <div class="search">
-      <Search />
+      <Search @position="position($event)" />
     </div>
   </div>
 </template>
@@ -102,6 +18,18 @@ export default {
   components: {
     Map,
     Search,
+  },
+  data() {
+    return {
+      lat: 0,
+      lon: 0,
+    };
+  },
+  methods: {
+    position(event: any) {
+      this.lat = event.lat;
+      this.lon = event.lon;
+    },
   },
 };
 </script>
@@ -118,28 +46,42 @@ export default {
   height: 100vh;
   display: flex;
   flex-direction: row;
-  
-  .map, .search {
-    width: 50%;
-  }
-}
 
-.map {
-  height: 100vh;
+  .map,
+  .search {
+    width: 50%;
+    height: 100vh;
+  }
 }
 
 .search {
-  background-color: grey;
+  background: rgb(31, 31, 31);
 }
 
-@media screen and (max-width: 768px) {
+// Removing Scrollbar
+body {
+  -ms-overflow-style: none; /* for Internet Explorer, Edge */
+  scrollbar-width: none; /* for Firefox */
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+  }
+}
+
+@media screen and (max-width: 912px) {
   .container {
     flex-direction: column;
 
-    .map, .search {
+    .map,
+    .search {
       width: 100%;
       height: 50vh;
->>>>>>> 25925512a4670f8e971cb2a841eff4b4b2707003
     }
   }
+
+  .list {
+    height: calc(100vh - (50vh + 146px));
+    overflow-y: scroll;
+  }
+}
 </style>
